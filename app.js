8bf256975a42fae4effa7db2860d46509aeee89c
Atharva Gulhane <atharva1610@gmail.com>
2020-12-27 18:37:46 +0530

    port change

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.use((err,req,res,next)=>{
     
 })
 
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log("App started at port 3000");
